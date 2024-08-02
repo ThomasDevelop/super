9c7958c52327d877612bf4df32146709e3b57e15
thomas <116232070+ThomasDevelop@users.noreply.github.com>
2024-08-02 13:53:58 -0300

    Delete src directory

--- src/main/java/com/SuperShop/Super/dto/AdminListaDTO.java
+++
@@ -1,8 +0,0 @@
-package com.SuperShop.Super.dto;
-
-import lombok.Data;
-@Data
-public class AdminListaDTO {
-    private String id;
-    private String nome;
-}
--- src/main/java/com/SuperShop/Super/exception/AdminIdInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package com.SuperShop.Super.exception;
-
-public class AdminIdInvalidoException extends RuntimeException {
-    public AdminIdInvalidoException(String message) {
-        super(message);
-    }
-}
